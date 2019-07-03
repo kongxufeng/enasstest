a3de673d383fc33bbdcd0d133649fc8c84b47fa2
kongxufeng <kongxufeng@htyunwang.com>
2019-07-03 17:46:25 +0800

    修改测试地址2

--- src/main/java/com/enass/comm/Constants.java
+++ src/main/java/com/enass/comm/Constants.java
@@ -4,9 +4,9 @@ public class Constants {
 	private Constants() {
 	}
 	//测试地址
- 	public static final String BasicURL = "http://39.98.238.23/#/serviceHome";
+ 	//public static final String BasicURL = "http://39.98.238.23/#/serviceHome";
 	//正式地址
-	//public static final String BasicURL = "http://enass.casicloud.com/#/serviceHome";
+	public static final String BasicURL = "http://enass.casicloud.com/#/serviceHome";
 
 	public static final String VerifyCODE = "HT09";
 	public static final String VerifyPhoneCODE = "0909";
